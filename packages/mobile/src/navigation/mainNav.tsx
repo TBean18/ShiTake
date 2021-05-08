5f1e11266a48955306f9a6cf86842a6725103ae4
Taylor Bean <TBean@Knights.ucf.edu>
2021-05-08 01:49:39 -0400

    init navigation

---
+++ packages/mobile/src/navigation/mainNav.tsx
@@ -0,0 +1,28 @@
+import React, { FC, useEffect, useState } from "react";
+import firebase from "firebase";
+import { NavigationContainer } from "@react-navigation/native";
+import AppStack from "./appstack";
+import AuthStack from "./authstack";
+
+const MainNav: FC = () => {
+  const [user, setUser] = useState<any>(null);
+
+  const bootstrap = () => {
+    firebase.auth().onAuthStateChanged((_user) => {
+      if (_user) {
+        setUser(_user);
+      }
+    });
+  };
+
+  useEffect(() => {
+    bootstrap();
+  }, []);
+  return (
+    <NavigationContainer>
+      {user != null ? <AppStack /> : <AuthStack />}
+    </NavigationContainer>
+  );
+};
+
+export default MainNav;
