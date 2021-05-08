6b4148f645653ae849732ea260def004c683a05d
Taylor Bean <TBean@Knights.ucf.edu>
2021-05-08 01:48:25 -0400

    firebase config

---
+++ packages/mobile/src/constants/Firebase.ts
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyB8goCidBIuFM3n1aQ2pMAPV_SC7lo7k-0",
+  authDomain: "shitake-b3a79.firebaseapp.com",
+  projectId: "shitake-b3a79",
+  storageBucket: "shitake-b3a79.appspot.com",
+  messagingSenderId: "757074288438",
+  appId: "1:757074288438:web:c43bab7dd2a5c82a8b1d97",
+  measurementId: "G-RVVF34QB6Y",
+};
+
+firebase.initializeApp(firebaseConfig);
